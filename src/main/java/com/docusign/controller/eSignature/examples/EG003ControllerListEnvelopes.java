7046e6fdb401c04ee19b6be03e193bebf0c029f4
Aaron Wilde <52012222+AaronWDS@users.noreply.github.com>
2020-08-26 09:04:20 -0700

    Devdocs 3174 (#4)
    * baseline fix of moving to eSignature folder and setting quickstart variable
    
    * baseline fix of moving to eSignature folder and setting quickstart variable
    
    * point to 3.8.0 sdik and remove patch

---
+++ src/main/java/com/docusign/controller/eSignature/examples/EG003ControllerListEnvelopes.java
@@ -0,0 +1,60 @@
+package com.docusign.controller.eSignature.examples;
+
+import com.docusign.DSConfiguration;
+import com.docusign.esign.api.EnvelopesApi;
+import com.docusign.esign.api.EnvelopesApi.ListStatusChangesOptions;
+import com.docusign.esign.client.ApiException;
+import com.docusign.model.DoneExample;
+import com.docusign.model.Session;
+import com.docusign.model.User;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * List envelopes in the user's account.<br />
+ * List the envelopes created in the last 30 days. This example demonstrates
+ * how to query DocuSign about envelopes sent by the current user.
+ */
+@Controller
+@RequestMapping("/eg003")
+public class EG003ControllerListEnvelopes extends AbstractController {
+
+    private static final int FROM_DATE_OFFSET_DAYS = 30;
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+    private final Session session;
+    private final User user;
+
+
+    @Autowired
+    public EG003ControllerListEnvelopes(DSConfiguration config, Session session, User user) {
+        super(config, "eg003", "List envelopes");
+        this.session = session;
+        this.user = user;
+    }
+
+    @Override
+    // ***DS.snippet.0.start
+    protected Object doWork(WorkArguments args, ModelMap model, HttpServletResponse response) throws ApiException {
+        EnvelopesApi envelopesApi = createEnvelopesApi(session.getBasePath(), user.getAccessToken());
+        ListStatusChangesOptions options = envelopesApi.new ListStatusChangesOptions();
+        LocalDate date = LocalDate.now().minusDays(FROM_DATE_OFFSET_DAYS);
+        options.setFromDate(DATE_FORMATTER.format(date));
+
+        DoneExample.createDefault(title)
+                .withMessage("Results from the Envelopes::listStatusChanges method:")
+                .withJsonObject(envelopesApi.listStatusChanges(session.getAccountId(), options))
+                .addToModel(model);
+        return DONE_EXAMPLE_PAGE;
+    }
+    // ***DS.snippet.0.end
+}
---
+++ src/main/java/com/docusign/controller/eSignature/examples/EG006ControllerEnvelopeDocs.java
@@ -0,0 +1,83 @@
+package com.docusign.controller.eSignature.examples;
+
+import com.docusign.DSConfiguration;
+import com.docusign.esign.api.EnvelopesApi;
+import com.docusign.esign.client.ApiException;
+import com.docusign.esign.model.EnvelopeDocument;
+import com.docusign.esign.model.EnvelopeDocumentsResult;
+import com.docusign.model.DoneExample;
+import com.docusign.model.EnvelopeDocumentInfo;
+import com.docusign.model.Session;
+import com.docusign.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * List an envelope's documents.<br />
+ * A <em>Certificate of Completion</em> document is also associated with every
+ * envelope. This method is often used to dynamically create a list of an
+ * envelope's documents in preparation for enabling your user to download one
+ * or more of the documents.
+ */
+@Controller
+@RequestMapping("/eg006")
+public class EG006ControllerEnvelopeDocs extends AbstractController {
+
+    private final Session session;
+    private final User user;
+
+    @Autowired
+    public EG006ControllerEnvelopeDocs(DSConfiguration config, Session session, User user) {
+        super(config, "eg006", "List envelope documents");
+        this.session = session;
+        this.user = user;
+    }
+
+    @Override
+    protected void onInitModel(WorkArguments args, ModelMap model) throws ApiException {
+        super.onInitModel(args, model);
+        model.addAttribute(MODEL_ENVELOPE_OK, StringUtils.isNotBlank(session.getEnvelopeId()));
+    }
+
+    @Override
+    // ***DS.snippet.0.start
+    protected Object doWork(WorkArguments args, ModelMap model, HttpServletResponse response) throws ApiException {
+        EnvelopesApi envelopesApi = createEnvelopesApi(session.getBasePath(), user.getAccessToken());
+
+        // Step 1. List the envelope's documents
+        EnvelopeDocumentsResult result = envelopesApi.listDocuments(session.getAccountId(), session.getEnvelopeId());
+
+        // Step 2. Process results
+        // Save the envelopeId and its list of documents in the session so
+        // they can be used in example 7 (download a document)
+        List<EnvelopeDocumentInfo> envelopeDocItems = new ArrayList<>();
+        session.setEnvelopeDocuments(envelopeDocItems);
+        envelopeDocItems.add(new EnvelopeDocumentInfo("Combined", "content", "combined"));
+        envelopeDocItems.add(new EnvelopeDocumentInfo("Zip archive", "zip", "archive"));
+
+        for (EnvelopeDocument doc : result.getEnvelopeDocuments()) {
+            String documentName = doc.getName();
+            if (StringUtils.equals(doc.getDocumentId(), "certificate")) {
+                documentName = "Certificate of completion";
+            }
+            envelopeDocItems.add(new EnvelopeDocumentInfo(documentName, doc.getType(), doc.getDocumentId()));
+        }
+
+        DoneExample.createDefault(title)
+            .withJsonObject(result)
+            .withMessage("Results from the EnvelopeDocuments::list method:")
+            .addToModel(model);
+        return DONE_EXAMPLE_PAGE;
+    }
+    // ***DS.snippet.0.end
+}
