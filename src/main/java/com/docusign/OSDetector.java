41bb6282c69952ecdfe3bbc273b7a27d7a486257
Paige Rossi <paige.rossi@docusign.com>
2020-12-18 12:39:25 -0800

    detecting the user's os and acting accordingly

---
+++ src/main/java/com/docusign/OSDetector.java
@@ -0,0 +1,21 @@
+package com.docusign;
+
+public class OSDetector
+{
+    private static boolean isWindows;
+    private static boolean isLinux;
+    private static boolean isMac;
+
+    static
+    {
+        String os = System.getProperty("os.name").toLowerCase();
+        isWindows = os.contains("win");
+        isLinux = os.contains("nux") || os.contains("nix");
+        isMac = os.contains("mac");
+    }
+
+    public static boolean isWindows() { return isWindows; }
+    public static boolean isLinux() { return isLinux; }
+    public static boolean isMac() { return isMac; };
+
+}
