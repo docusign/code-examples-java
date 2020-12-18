8ba46d2b3f958d61453b5c257f67146f7a25ae20
Paige Rossi <paige.rossi@docusign.com>
2020-12-18 12:39:25 -0800

    detecting the user's os and acting accordingly

--- src/main/java/com/docusign/App.java
+++ src/main/java/com/docusign/App.java
@@ -12,5 +12,20 @@ public class App {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(App.class, args);
+        openHomePage();
     }
+
+    private static void openHomePage() throws IOException {
+        if (OSDetector.isMac()){
+            Runtime rt = Runtime.getRuntime();
+            String[] arguments = { "osascript", "-e", "open location \"" + "http://localhost:5000" + "\"" };
+            rt.exec(arguments);
+        }
+        else {
+           Runtime rt = Runtime.getRuntime();
+           rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080");
+        }
+
+    }
+
 }
