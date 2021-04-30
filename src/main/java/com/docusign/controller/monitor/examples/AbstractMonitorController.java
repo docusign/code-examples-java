1e517a8f94850c292e79543f9947468ad3c24a10
annahileta <anna.hileta@sigma.software>
2021-04-30 18:00:24 +0300

    Moved authorization check to AbstractMonitorController

--- src/main/java/com/docusign/controller/monitor/examples/AbstractMonitorController.java
+++ src/main/java/com/docusign/controller/monitor/examples/AbstractMonitorController.java
@@ -1,16 +1,29 @@
 package com.docusign.controller.monitor.examples;
 
 import com.docusign.DSConfiguration;
+import com.docusign.WebSecurityConfig;
 import com.docusign.core.controller.AbstractController;
+import com.docusign.core.model.AuthType;
+import com.docusign.core.model.Session;
+import com.docusign.core.security.jwt.JWTAuthorizationCodeResourceDetails;
+import com.docusign.core.security.jwt.JWTOAuth2RestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
 
 /**
- * Abstract base class for all Click controllers.
+ * Abstract base class for all Monitor controllers.
  */
 @Controller
 public abstract class AbstractMonitorController extends AbstractController {
 
     private static final String EXAMPLE_PAGES_PATH = "pages/monitor/examples/";
+
+    @Qualifier("oauth2ClientContext")
+    @Autowired
+    private OAuth2ClientContext oAuth2ClientContext;
 
     public AbstractMonitorController(DSConfiguration config, String exampleName, String title) {
         super(config, exampleName, title);
@@ -18,6 +31,16 @@ public abstract class AbstractMonitorController extends AbstractController {
 
     protected String getExamplePagesPath() {
         return AbstractMonitorController.EXAMPLE_PAGES_PATH;
+    }
+
+    protected void ensureUsageOfJWTToken(String accessToken, Session session) {
+        if (session.getAuthTypeSelected() != AuthType.JWT || accessToken.isEmpty()){
+            WebSecurityConfig securityConfig = new WebSecurityConfig();
+            JWTAuthorizationCodeResourceDetails jwtCodeGrantClient = securityConfig.jwtCodeGrantClient();
+
+            OAuth2RestTemplate restTemplate = new JWTOAuth2RestTemplate(jwtCodeGrantClient, oAuth2ClientContext);
+            accessToken = restTemplate.getAccessToken().toString();
+        }
     }
 }
 
