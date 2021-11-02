157e8533a317f93b412d0593820ef44b2ccab16e
annahileta <anna.hileta@sigma.software>
2021-11-02 14:59:17 +0200

    Moved writing export to file into service

--- src/main/java/com/docusign/controller/admin/examples/A003BulkExportUserData.java
+++ src/main/java/com/docusign/controller/admin/examples/A003BulkExportUserData.java
@@ -9,23 +9,12 @@ import com.docusign.core.model.DoneExample;
 import com.docusign.core.model.Session;
 import com.docusign.core.model.User;
 import com.docusign.services.admin.examples.BulkExportUserDataService;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.MediaType;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Paths;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -65,42 +54,11 @@ public class A003BulkExportUserData extends AbstractAdminController {
         String csvUri = BulkExportUserDataService.bulkExportUserData(bulkExportsApi, organizationId, bulkList.getId());
         // Step 4 end
 
-        // Step 5 start
-        URL request_url = new URL(csvUri);
-
-        // Send Web request to download and save the exported CSV data
-        HttpsURLConnection.setFollowRedirects(true); // Defaults to true
-        HttpsURLConnection http_conn = (HttpsURLConnection) request_url.openConnection();
-        http_conn.setRequestProperty(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON);
-        http_conn.setRequestProperty(HttpHeaders.CONTENT_TYPE, "mutlipart/form-data");
-        http_conn.setRequestProperty(HttpHeaders.AUTHORIZATION, BEARER_AUTHENTICATION + user.getAccessToken());
-        http_conn.setInstanceFollowRedirects(true);
-
-        ClassLoader rawPath = Thread.currentThread().getContextClassLoader();
-        String rootDir = Paths.get(Objects.requireNonNull(rawPath.getResource("")).toURI()).toString();
-        // Set the save file path in the project root instead of the compiled folder and
-        // force forward slash scheme
-        rootDir = StringUtils.remove(rootDir, "target\\classes");
-        rootDir = StringUtils.remove(rootDir, "file:/");
-        rootDir = StringUtils.replace(rootDir, "\\", "/");
-
-        // opens input stream from the HTTP connection
-        InputStream inputStream = http_conn.getInputStream();
-        String saveFilePath = rootDir + "src/main/resources/ExportedUserData.csv";
-
-        // opens an output stream to save into file
-        FileOutputStream outputStream = FileUtils.openOutputStream(new File(saveFilePath));
-
-        int bytesRead;
-        byte[] buffer = new byte[BUFFER_SIZE];
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            outputStream.write(buffer, 0, bytesRead);
-        }
-
-        outputStream.close();
-        inputStream.close();
-        http_conn.disconnect();
-        // Step 5 end
+        String saveFilePath = BulkExportUserDataService.moveUserListExportToFile(
+                csvUri,
+                BEARER_AUTHENTICATION,
+                user.getAccessToken(),
+                BUFFER_SIZE);
 
         OrganizationExportsResponse results = BulkExportUserDataService.bulkExportsUserData(
                 bulkExportsApi,
