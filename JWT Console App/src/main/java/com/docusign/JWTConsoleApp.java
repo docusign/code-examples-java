544a4c59383136fcd0b8c012bd8d5a8bbfbacdfc
Inbar Gazit <inbar.gazit@docusign.com>
2022-02-02 14:53:28 -0800

    Adding initial files for this

---
+++ JWT Console App/src/main/java/com/docusign/JWTConsoleApp.java
@@ -0,0 +1,32 @@
+package docusign.jwtconsoleapp;
+
+import com.opencsv.CSVReaderBuilder;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.stat.StatUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+/**
+ * Starter class for JWTConsoleApp application.
+ */
+
+public class JWTConsoleApp {
+
+    /**
+     * Application entry point.
+     *
+     * @param args application command line arguments
+     */
+    public static void main(String[] args) {
+	System.out.println("Hello World!");
+    }
+}
