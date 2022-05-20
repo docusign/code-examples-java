9379a0d91bcaa20d907b1304f34cf1a878a1f0c6
Anna Hileta <80112359+annahileta@users.noreply.github.com>
2022-05-20 11:12:38 +0300

    Added post web query code example (#60)
    * Added a post web query code example
    
    * Text updates
    
    * fixed formatting
    
    * removed unused imports

---
+++ src/main/java/com/docusign/controller/monitor/examples/M002WebQueryEndpoint.java
@@ -0,0 +1,81 @@
+package com.docusign.controller.monitor.examples;
+
+import com.docusign.DSConfiguration;
+import com.docusign.common.WorkArguments;
+import com.docusign.controller.monitor.services.WebQueryEndpointService;
+import com.docusign.core.model.DoneExample;
+import com.docusign.core.model.Session;
+import com.docusign.core.model.User;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Get monitoring data<br />
+ * This example demonstrates how to get monitoring data from the Monitor API.
+ */
+@Controller
+@RequestMapping("/m002")
+public class M002WebQueryEndpoint extends AbstractMonitorController {
+
+    private static final String MODEL_START_DATE = "startDate";
+    private static final String MODEL_END_DATE = "endDate";
+    private static final int FROM_DATE_OFFSET_DAYS = 10;
+    private static final int FROM_DATE_FORWARD_DAYS = 1;
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private final Session session;
+    private final User user;
+
+    @Autowired
+    public M002WebQueryEndpoint(DSConfiguration config, Session session, User user) {
+        super(config, "m002", "Query monitoring data with filters");
+        this.session = session;
+        this.user = user;
+    }
+
+    @Override
+    protected void onInitModel(WorkArguments args, ModelMap model) throws Exception {
+        super.onInitModel(args, model);
+
+        LocalDate endDate = LocalDate.now().plusDays(FROM_DATE_FORWARD_DAYS);
+        LocalDate startDate = LocalDate.now().minusDays(FROM_DATE_OFFSET_DAYS);
+
+        model.addAttribute(MODEL_START_DATE, DATE_FORMATTER.format(startDate));
+        model.addAttribute(MODEL_END_DATE, DATE_FORMATTER.format(endDate));
+    }
+
+    @Override
+    protected Object doWork(WorkArguments args, ModelMap model, HttpServletResponse response) throws Exception {
+        String accessToken = this.user.getAccessToken();
+
+        // Check, if you are using the JWT authentication
+        // step 1 start
+        accessToken = ensureUsageOfJWTToken(accessToken, this.session);
+        String accountId = session.getAccountId();
+        // step 1 end
+
+        JSONObject queryResult = WebQueryEndpointService.postWebQueryMethod(
+                createDataSetApi(accessToken, this.session),
+                accountId,
+                args.getStartDate(),
+                args.getEndDate()
+        );
+
+        // Cleaning the data from unsupported symbols
+        String queryResultCleaned = queryResult.toString().replaceAll("'", "");
+
+        // Process results
+        DoneExample.createDefault(title)
+                .withMessage("Results from DataSet:postWebQuery method:")
+                .withJsonObject(queryResultCleaned)
+                .addToModel(model);
+
+        return DONE_EXAMPLE_PAGE;
+    }
+}
---
+++ src/main/java/com/docusign/controller/monitor/services/WebQueryEndpointService.java
@@ -0,0 +1,54 @@
+package com.docusign.controller.monitor.services;
+
+import com.docusign.monitor.api.DataSetApi;
+import com.docusign.monitor.model.AggregateResult;
+import com.docusign.monitor.model.AggregateResultResult;
+import com.docusign.monitor.model.WebQuery;
+import com.docusign.monitor.client.ApiException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+public final class WebQueryEndpointService {
+    private static final String API_VERSION = "2.0";
+    private static final String NAME_OF_DATASET = "monitor";
+    public static JSONObject postWebQueryMethod(
+            DataSetApi datasetApi,
+            String accountId,
+            String filterStartDate,
+            String filterEndDate) throws ApiException {
+        AggregateResult aggregateResult = datasetApi.postWebQuery(
+                API_VERSION,
+                NAME_OF_DATASET,
+                prepareWebQueryOptions(accountId, filterStartDate, filterEndDate));
+
+        AggregateResultResult methodResult = null;
+        List<AggregateResultResult> aggregateResultList = aggregateResult.getResult();
+
+        if (aggregateResultList != null && !aggregateResultList.isEmpty()) {
+            methodResult = aggregateResultList.get(0);
+        }
+
+        return new JSONObject(methodResult);
+    }
+
+    private static WebQuery prepareWebQueryOptions(String accountId, String filterStartDate, String filterEndDate) {
+        WebQuery webQuery = new WebQuery();
+        webQuery.addFiltersItem(new Object(){
+            public String FilterName = "Time";
+            public String BeginTime = filterStartDate;
+            public String EndTime = filterEndDate;
+        });
+        webQuery.addFiltersItem(new Object(){
+            public String FilterName = "Has";
+            public String ColumnName = "AccountId";
+            public String Value = accountId;
+        });
+        webQuery.addAggregationsItem(new Object(){
+            public String aggregationName = "Raw";
+            public String limit = "1";
+            public String[] orderby = new String[] { "Timestamp, desc" };
+        });
+        return webQuery;
+    }
+}
