dda1bc18fc180bd87585d9c44a290a59913bb028
annahileta <anna.hileta@sigma.software>
2022-08-12 11:53:29 +0300

    added models to map json file content

---
+++ src/main/java/com/docusign/core/model/manifestModels/CodeExampleText.java
@@ -0,0 +1,29 @@
+package com.docusign.core.model.manifestModels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeExampleText {
+    public int ExampleNumber;
+
+    public String ExampleName;
+
+    public String ExampleDescription;
+
+    public String ExampleDescriptionExtended;
+
+    public String PageTitle;
+
+    public String ResultsPageHeader;
+
+    public String ResultsPageText;
+
+    public String SkipForLanguages;
+
+    public List<LinkToAPIMethods> LinksToAPIMethod = new ArrayList<>();
+
+    public List<AdditionalPage> AdditionalPages = new ArrayList<>();
+}
---
+++ src/main/java/com/docusign/core/model/manifestModels/ManifestGroup.java
@@ -0,0 +1,13 @@
+package com.docusign.core.model.manifestModels;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManifestGroup {
+    public List<CodeExampleText> Examples = new ArrayList<>();
+
+    public String Name;
+}
