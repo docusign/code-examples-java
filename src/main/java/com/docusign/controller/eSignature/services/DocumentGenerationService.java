1ce94d01f3f4b7d86c2b766730faf03fe9745e77
Anna Hileta <80112359+annahileta@users.noreply.github.com>
2023-04-21 21:42:52 +0300

    Added a document generation code example (#99)
    * added a document generation code example
    
    * fixed code review comments
    
    * added more constants
    
    * updated the userflow
    
    * fixes after code review
    
    * fixes for a controller file
    
    * small fixes
    
    * Revert "small fixes"
    
    This reverts commit b08e8ae2df6dcb2b3fd8b5af94b6b8d6a39a62b7.
    
    * pr feedback
    
    ---------
    
    Co-authored-by: AaronWDS <aaron.wilde@docusign.com>

---
+++ src/main/java/com/docusign/controller/eSignature/services/DocumentGenerationService.java
@@ -0,0 +1,211 @@
+package com.docusign.controller.eSignature.services;
+
+import com.docusign.controller.eSignature.examples.EnvelopeHelpers;
+import com.docusign.esign.api.EnvelopesApi;
+import com.docusign.esign.api.TemplatesApi;
+import com.docusign.esign.client.ApiException;
+import com.docusign.esign.model.DocGenFormFieldResponse;
+import com.docusign.esign.model.DocGenFormFields;
+import com.docusign.esign.model.EnvelopeSummary;
+import com.docusign.esign.model.TemplateSummary;
+import com.docusign.esign.model.DocGenFormFieldRequest;
+import com.docusign.esign.model.Envelope;
+import com.docusign.esign.model.EnvelopeUpdateSummary;
+import com.docusign.esign.model.TemplateTabs;
+import com.docusign.esign.model.SignHere;
+import com.docusign.esign.model.DateSigned;
+import com.docusign.esign.model.DocGenFormField;
+import com.docusign.esign.model.EnvelopeDefinition;
+import com.docusign.esign.model.TemplateRole;
+import com.docusign.esign.model.EnvelopeTemplate;
+import com.docusign.esign.model.Signer;
+import com.docusign.esign.model.Recipients;
+import com.docusign.esign.model.Document;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Document generation code example
+ */
+public final class DocumentGenerationService {
+    public static final String CANDIDATE_NAME = "Candidate_Name";
+    public static final String MANAGER_NAME = "Manager_Name";
+    public static final String JOB_TITLE = "Job_Title";
+    public static final String SALARY = "Salary";
+    public static final String START_DATE = "Start_Date";
+    public static final String TEXT_BOX = "TextBox";
+    public static final String STRING_TRUE = "true";
+    public static final String ANCHOR_UNITS = "pixels";
+    public static final String DEFAULT_ID = "1";
+
+    public String generateDocument(
+            String accountId,
+            String candidateEmail,
+            String candidateName,
+            String managerName,
+            String jobTitle,
+            String salary,
+            String startDate,
+            String offerDocDocx,
+            EnvelopesApi envelopesApi,
+            TemplatesApi templatesApi
+    ) throws ApiException, IOException {
+        TemplateSummary template = templatesApi.createTemplate(accountId, makeTemplate());
+        String templateId = template.getTemplateId();
+
+        templatesApi.updateDocument(accountId, templateId, DEFAULT_ID, addDocumentTemplate(offerDocDocx));
+        templatesApi.createTabs(accountId, templateId, DEFAULT_ID, prepareTabs());
+
+        EnvelopeSummary envelopeSummary = envelopesApi.createEnvelope(
+                accountId,
+                makeEnvelope(candidateEmail, candidateName, template.getTemplateId()));
+        String envelopeId = envelopeSummary.getEnvelopeId();
+
+        DocGenFormFieldResponse formFieldResponse = envelopesApi.getEnvelopeDocGenFormFields(accountId, envelopeId);
+        String documentId = "";
+        if (!formFieldResponse.getDocGenFormFields().isEmpty()) {
+            DocGenFormFields docGenFormFields = formFieldResponse.getDocGenFormFields().get(0);
+            if (docGenFormFields != null){
+                documentId = docGenFormFields.getDocumentId();
+            }
+        }
+
+        DocGenFormFieldRequest formFields = getFormFields(
+                documentId,
+                candidateName,
+                managerName,
+                jobTitle,
+                salary,
+                startDate);
+
+        envelopesApi.updateEnvelopeDocGenFormFields(accountId, envelopeId, formFields);
+
+        Envelope envelope = new Envelope();
+        envelope.setStatus(EnvelopeHelpers.ENVELOPE_STATUS_SENT);
+
+        EnvelopeUpdateSummary envelopeUpdateSummary = envelopesApi.update(accountId, envelopeId, envelope);
+
+        return envelopeUpdateSummary.getEnvelopeId();
+    }
+
+    private TemplateTabs prepareTabs() {
+        SignHere signHere = createSignHere();
+        DateSigned dateSigned = createDateSigned();
+
+        TemplateTabs templateTabs = new TemplateTabs();
+        templateTabs.setSignHereTabs(Collections.singletonList(signHere));
+        templateTabs.setDateSignedTabs(Collections.singletonList(dateSigned));
+
+        return templateTabs;
+    }
+
+    private SignHere createSignHere() {
+        SignHere signHere = new SignHere();
+
+        signHere.setAnchorString("Employee Signature");
+        signHere.setAnchorUnits(ANCHOR_UNITS);
+        signHere.setAnchorXOffset("5");
+        signHere.setAnchorYOffset("-22");
+
+        return signHere;
+    }
+
+    public static DateSigned createDateSigned() {
+        DateSigned dateSigned = new DateSigned();
+
+        dateSigned.setAnchorString("Date");
+        dateSigned.setAnchorUnits(ANCHOR_UNITS);
+        dateSigned.setAnchorYOffset("-22");
+
+        return dateSigned;
+    }
+
+    private DocGenFormFieldRequest getFormFields(
+            String documentId,
+            String candidateName,
+            String managerName,
+            String jobTitle,
+            String salary,
+            String startDate) {
+        DocGenFormField candidateNameField = new DocGenFormField();
+        candidateNameField.setName(CANDIDATE_NAME);
+        candidateNameField.setValue(candidateName);
+
+        DocGenFormField managerNameField = new DocGenFormField();
+        managerNameField.setName(MANAGER_NAME);
+        managerNameField.setValue(managerName);
+
+        DocGenFormField jobTitleField = new DocGenFormField();
+        jobTitleField.setName(JOB_TITLE);
+        jobTitleField.setValue(jobTitle);
+
+        DocGenFormField salaryField = new DocGenFormField();
+        salaryField.setName(SALARY);
+        salaryField.setValue(salary);
+
+        DocGenFormField startDateField = new DocGenFormField();
+        startDateField.setName(START_DATE);
+        startDateField.setValue(startDate);
+
+        DocGenFormFields formFields = new DocGenFormFields();
+        formFields.setDocGenFormFieldList(Arrays.asList(
+                candidateNameField,
+                managerNameField,
+                jobTitleField,
+                salaryField,
+                startDateField));
+        formFields.setDocumentId(documentId);
+
+        DocGenFormFieldRequest docGenFormFieldRequest = new DocGenFormFieldRequest();
+        docGenFormFieldRequest.setDocGenFormFields(Collections.singletonList(formFields));
+
+        return docGenFormFieldRequest;
+    }
+
+    private EnvelopeDefinition makeEnvelope(String candidateEmail, String candidateName, String templateId) {
+        TemplateRole signerRole = new TemplateRole();
+        signerRole.setName(candidateName);
+        signerRole.setEmail(candidateEmail);
+        signerRole.setRoleName(EnvelopeHelpers.SIGNER_ROLE_NAME);
+
+        EnvelopeDefinition envelopeDefinition = new EnvelopeDefinition();
+        envelopeDefinition.setTemplateRoles(Collections.singletonList(signerRole));
+        envelopeDefinition.setStatus(EnvelopeHelpers.ENVELOPE_STATUS_CREATED);
+        envelopeDefinition.setTemplateId(templateId);
+
+        return envelopeDefinition;
+    }
+
+    private EnvelopeTemplate makeTemplate() {
+        Signer signer = new Signer();
+        signer.setRoleName(EnvelopeHelpers.SIGNER_ROLE_NAME);
+        signer.setRecipientId(DEFAULT_ID);
+        signer.setRoutingOrder("1");
+
+        Recipients recipients = new Recipients();
+        recipients.setSigners(Collections.singletonList(signer));
+
+        EnvelopeTemplate template = new EnvelopeTemplate();
+        template.setEmailSubject("Please sign this document");
+        template.setName("Example Template");
+        template.setDescription("Example template created via the API");
+        template.setRecipients(recipients);
+        template.setStatus(EnvelopeHelpers.ENVELOPE_STATUS_CREATED);
+
+        return template;
+    }
+
+    private static EnvelopeDefinition addDocumentTemplate(String offerDocDocx) throws IOException {
+        String documentName = "OfferLetterDemo.docx";
+        Document document = EnvelopeHelpers.createDocumentFromFile(offerDocDocx, documentName,DEFAULT_ID);
+        document.setOrder("1");
+        document.pages("1");
+
+        EnvelopeDefinition envelopeDefinition = new EnvelopeDefinition();
+        envelopeDefinition.setDocuments(Collections.singletonList(document));
+
+        return envelopeDefinition;
+    }
+}
---
+++ src/main/webapp/WEB-INF/templates/views/pages/esignature/examples/eg042.jsp
@@ -0,0 +1,114 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<jsp:include page="../../../partials/head.jsp"/>
+
+<c:set var="formNumber" value="0" scope="page" />
+<c:set var="candidateEmailIndex" value="0" scope="page" />
+<c:set var="candidateNameIndex" value="1" scope="page" />
+<c:set var="managerNameIndex" value="2" scope="page" />
+<c:set var="jobTitleIndex" value="3" scope="page" />
+<c:set var="salaryIndex" value="4" scope="page" />
+<c:set var="startDateIndex" value="5" scope="page" />
+
+<h4>${example.getExampleName()}</h4>
+<p>${example.getExampleDescription()}</p>
+<c:if test="${showDoc}">
+    <p><a target='_blank' href='${documentation}'>Documentation</a> about this example.</p>
+</c:if>
+
+<jsp:include page="../../links_to_api_methods.jsp" />
+
+<p>
+    ${viewSourceFile}
+</p>
+
+<form class="eg" action="" method="post" data-busy="form">
+    <p>
+        ${example.getForms().get(formNumber).getFormName()}
+    </p>
+    <div class="form-group">
+        <label for="candidateEmail">
+            ${example.getForms().get(formNumber).getInputs().get(candidateEmailIndex).getInputName()}
+        </label>
+
+        <input type="email"
+               class="form-control"
+               id="candidateEmail"
+               name="candidateEmail"
+               aria-describedby="emailHelp"
+               placeholder="${example.getForms().get(formNumber).getInputs().get(candidateEmailIndex).getInputPlaceholder()}"
+               required>
+
+        <small id="emailHelp" class="form-text text-muted">
+            ${launcherTexts.getHelpingTexts().getEmailWontBeShared()}
+        </small>
+    </div>
+    <div class="form-group">
+      <label for="candidateName">
+          ${example.getForms().get(formNumber).getInputs().get(candidateNameIndex).getInputName()}
+      </label>
+
+      <input type="text"
+             class="form-control"
+             id="candidateName"
+             placeholder="${example.getForms().get(formNumber).getInputs().get(candidateNameIndex).getInputPlaceholder()}"
+             name="candidateName"
+             required>
+    </div>
+    <div class="form-group">
+      <label for="managerName">
+          ${example.getForms().get(formNumber).getInputs().get(managerNameIndex).getInputName()}
+      </label>
+
+      <input type="tel"
+             class="form-control"
+             id="managerName"
+             name="managerName"
+             aria-describedby="accessHelp"
+             placeholder="${example.getForms().get(formNumber).getInputs().get(managerNameIndex).getInputPlaceholder()}"
+             required/>
+    </div>
+
+    <div class="form-group">
+        <label for="jobTitle">
+            ${example.getForms().get(formNumber).getInputs().get(jobTitleIndex).getInputName()}
+        </label>
+
+        <select class="form-control" id="jobTitle" name="jobTitle">
+            <option value="Software Engineer">Software Engineer</option>
+            <option value="Product Manager">Product Manager</option>
+            <option value="Sales Representative">Sales Representative</option>
+        </select>
+    </div>
+
+    <div class="form-group">
+        <label for="salary">
+            ${example.getForms().get(formNumber).getInputs().get(salaryIndex).getInputName()}
+        </label>
+
+        <input type="text"
+               class="form-control"
+               id="salary"
+               placeholder="${example.getForms().get(formNumber).getInputs().get(salaryIndex).getInputPlaceholder()}"
+               name="salary"
+               required>
+    </div>
+
+    <div class="form-group">
+        <label for="startDate">
+            ${example.getForms().get(formNumber).getInputs().get(startDateIndex).getInputName()}
+        </label>
+
+        <input type="date"
+               class="form-control"
+               id="startDate"
+               name="startDate"
+               aria-describedby="accessHelp"
+               placeholder="${example.getForms().get(formNumber).getInputs().get(startDateIndex).getInputPlaceholder()}"
+               required/>
+    </div>
+
+    <input type="hidden" name="_csrf" value="${csrfToken}">
+    <button type="submit" class="btn btn-docu">${launcherTexts.getSubmitButton()}</button>
+</form>
+
+<jsp:include page="../../../partials/foot.jsp"/>
