894be62af1ad2e9c8e2991d69e7740db12e79f71
Karissa Jacobsen <karissa.jacobsen@docusign.com>
2023-10-13 15:39:40 -0700

    DEVDOCS-5554 - codeDepot tags

--- src/main/java/com/docusign/controller/admin/services/UserDataManagementService.java
+++ src/main/java/com/docusign/controller/admin/services/UserDataManagementService.java
@@ -51,13 +51,13 @@ public class UserDataManagementService {
 			memberships.accountId(user.getMemberships().get(0).getAccountId());
 		}
 
-		//ds-snippet-start:Admin11Step3
+		//ds-snippet-start:Admin10Step3
 		IndividualUserDataRedactionRequest userRedactionRequest = new IndividualUserDataRedactionRequest();
 		userRedactionRequest.setUserId(user.getId());
 		userRedactionRequest.setMemberships(Arrays.asList(memberships));
-		//ds-snippet-end:Admin11Step3
-		//ds-snippet-start:Admin11Step4
+		//ds-snippet-end:Admin10Step3
+		//ds-snippet-start:Admin10Step4
 		return organizationsApi.redactIndividualUserData(organizationId, userRedactionRequest);
-		//ds-snippet-end:Admin11Step4
+		//ds-snippet-end:Admin10Step4
 	}
 }
