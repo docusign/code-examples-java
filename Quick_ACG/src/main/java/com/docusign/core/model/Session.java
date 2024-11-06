dc2a971aef333a753136a23561c31d70e6024e87
anna <anna.hileta@sigma.software>
2024-11-06 13:47:35 +0100

    added modification to support PKCE authorization in Quick ACG

--- Quick_ACG/src/main/java/com/docusign/core/model/Session.java
+++ Quick_ACG/src/main/java/com/docusign/core/model/Session.java
@@ -1,5 +1,6 @@
 package com.docusign.core.model;
 
+import com.docusign.esign.client.auth.OAuth;
 import lombok.Data;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
@@ -7,31 +8,26 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.UUID;
 
 @Component
-@Scope(value = WebApplicationContext.SCOPE_SESSION,
-        proxyMode = ScopedProxyMode.TARGET_CLASS)
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Data
 public class Session implements Serializable {
     private static final long serialVersionUID = 2695379118371574037L;
 
+    public Long tokenExpirationTime;
+
     private String accountId;
+
     private String accountName;
+
     private String basePath;
-    private String statusCFR;
-    private String roomsBasePath;
+
     private String envelopeId;
-    private String templateId;
-    private String templateName;
-    private String permissionProfileId;
-    private String permissionProfileName;
-    private String apiIndexPath;
-    private boolean refreshToken = false;
-    private String clickwrapId;
-    private String clickwrapVersionNumber;
-    private String exportId;
-    private String importId;
-    private UUID orgId;
-    public UUID bulkListId;
+
+    private String statusCFR;
+
+    private Boolean isPKCEWorking = true;
 }
