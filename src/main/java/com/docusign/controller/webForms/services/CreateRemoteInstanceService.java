c0f09fe5bff2fe8c3ba2cbc1c53daa41563784dd
anna <anna.hileta@sigma.software>
2025-09-23 14:47:48 +0200

    added fix for file loading in case of building with war command

--- src/main/java/com/docusign/controller/webForms/services/CreateRemoteInstanceService.java
+++ src/main/java/com/docusign/controller/webForms/services/CreateRemoteInstanceService.java
@@ -9,12 +9,14 @@ import com.docusign.webforms.client.ApiException;
 import com.docusign.webforms.model.*;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 
 public final class CreateRemoteInstanceService {
 
@@ -25,10 +27,9 @@ public final class CreateRemoteInstanceService {
     private static final String UNITS = "pixels";
 
     public static WebFormSummaryList getFormsByName(
-        ApiClient apiClient,
-        String userAccessToken,
-        String name
-    ) throws ApiException {
+            ApiClient apiClient,
+            String userAccessToken,
+            String name) throws ApiException {
         FormManagementApi formManagementApi = new FormManagementApi(apiClient);
         var option = formManagementApi.new ListFormsOptions();
         option.setSearch(name);
@@ -38,47 +39,42 @@ public final class CreateRemoteInstanceService {
 
     public static void addTemplateIdToForm(String fileName, String templateId) {
         try {
-            var resourceUrl = CreateRemoteInstanceService.class.getClassLoader().getResource(fileName);
-            if (resourceUrl == null) {
-                throw new IllegalArgumentException("File not found in resources: " + fileName);
-            }
+            ClassPathResource resource = new ClassPathResource(fileName);
+            byte[] buffer = StreamUtils.copyToByteArray(resource.getInputStream());
 
-            Path templateFile = Paths.get(resourceUrl.toURI());
-            String fileContent = Files.readString(templateFile);
+            String fileContent = new String(buffer);
             String modifiedContent = fileContent.replace("template-id", templateId);
 
             Path basePath = Paths.get(System.getProperty("user.dir"), "demo_documents");
             Files.createDirectories(basePath);
             Files.writeString(basePath.resolve(fileName), modifiedContent);
-        } catch (IOException | URISyntaxException ex) {
+        } catch (IOException ex) {
             System.out.println("An error occurred: " + ex.getMessage());
         }
     }
 
     public static WebFormInstance createInstance(
-        ApiClient apiClient,
-        String accountId,
-        String formId,
-        String signerEmail,
-        String signerName
-    ) throws ApiException {
+            ApiClient apiClient,
+            String accountId,
+            String formId,
+            String signerEmail,
+            String signerName) throws ApiException {
         WebFormValues formValues = new WebFormValues();
         formValues.putAll(Map.of(
-            "PhoneNumber", "555-555-5555",
-            "Yes", new String[]{ "Yes" },
-            "Company", "Tally",
-            "JobTitle", "Programmer Writer"
-        ));
+                "PhoneNumber", "555-555-5555",
+                "Yes", new String[] { "Yes" },
+                "Company", "Tally",
+                "JobTitle", "Programmer Writer"));
 
         var recipient = new CreateInstanceRequestBodyRecipients()
-            .email(signerEmail)
-            .name(signerName)
-            .roleName(ROLE_SIGNER);
+                .email(signerEmail)
+                .name(signerName)
+                .roleName(ROLE_SIGNER);
 
         CreateInstanceRequestBody options = new CreateInstanceRequestBody()
-            .sendOption(SendOption.NOW)
-            .formValues(formValues)
-            .recipients(List.of(recipient));
+                .sendOption(SendOption.NOW)
+                .formValues(formValues)
+                .recipients(List.of(recipient));
 
         return new FormInstanceManagementApi(apiClient).createInstance(accountId, formId, options);
     }
@@ -87,68 +83,63 @@ public final class CreateRemoteInstanceService {
         Document document = EnvelopeHelpers.createDocumentFromFile(documentPdf, "World_Wide_Web_Form", DOCUMENT_ID);
         Signer signer = createSigner();
         Recipients recipients = new Recipients().signers(List.of(signer));
-        
+
         return new EnvelopeTemplate()
-            .description("Example template created via the API")
-            .name(templateName)
-            .shared("false")
-            .documents(List.of(document))
-            .emailSubject("Please sign this document")
-            .recipients(recipients)
-            .status("created");
+                .description("Example template created via the API")
+                .name(templateName)
+                .shared("false")
+                .documents(List.of(document))
+                .emailSubject("Please sign this document")
+                .recipients(recipients)
+                .status("created");
     }
 
     private static Signer createSigner() {
         Signer signer = new Signer()
-            .roleName(ROLE_SIGNER)
-            .recipientId(DOCUMENT_ID)
-            .routingOrder("1");
+                .roleName(ROLE_SIGNER)
+                .recipientId(DOCUMENT_ID)
+                .routingOrder("1");
 
         signer.tabs(new Tabs()
-            .checkboxTabs(List.of(
-                new Checkbox()
-                    .documentId(DOCUMENT_ID)
-                    .tabLabel("Yes")
-                    .anchorString("/SMS/")
-                    .anchorUnits(UNITS)
-                    .anchorXOffset("0")
-                    .anchorYOffset("0")
-            ))
-            .signHereTabs(List.of(
-                new SignHere()
-                    .documentId(DOCUMENT_ID)
-                    .tabLabel("Signature")
-                    .anchorString("/SignHere/")
-                    .anchorUnits(UNITS)
-                    .anchorXOffset("20")
-                    .anchorYOffset("10")
-            ))
-            .textTabs(List.of(
-                createText("FullName", "/FullName/"),
-                createText("PhoneNumber", "/PhoneNumber/"),
-                createText("Company", "/Company/"),
-                createText("JobTitle", "/Title/")
-            ))
-            .dateSignedTabs(List.of(
-                new DateSigned()
-                    .documentId(DOCUMENT_ID)
-                    .tabLabel("DateSigned")
-                    .anchorString("/Date/")
-                    .anchorUnits(UNITS)
-                    .anchorXOffset("0")
-                    .anchorYOffset("0")
-            ))
-        );
+                .checkboxTabs(List.of(
+                        new Checkbox()
+                                .documentId(DOCUMENT_ID)
+                                .tabLabel("Yes")
+                                .anchorString("/SMS/")
+                                .anchorUnits(UNITS)
+                                .anchorXOffset("0")
+                                .anchorYOffset("0")))
+                .signHereTabs(List.of(
+                        new SignHere()
+                                .documentId(DOCUMENT_ID)
+                                .tabLabel("Signature")
+                                .anchorString("/SignHere/")
+                                .anchorUnits(UNITS)
+                                .anchorXOffset("20")
+                                .anchorYOffset("10")))
+                .textTabs(List.of(
+                        createText("FullName", "/FullName/"),
+                        createText("PhoneNumber", "/PhoneNumber/"),
+                        createText("Company", "/Company/"),
+                        createText("JobTitle", "/Title/")))
+                .dateSignedTabs(List.of(
+                        new DateSigned()
+                                .documentId(DOCUMENT_ID)
+                                .tabLabel("DateSigned")
+                                .anchorString("/Date/")
+                                .anchorUnits(UNITS)
+                                .anchorXOffset("0")
+                                .anchorYOffset("0"))));
         return signer;
     }
 
     private static Text createText(String tabLabel, String anchor) {
         return new Text()
-            .documentId(DOCUMENT_ID)
-            .tabLabel(tabLabel)
-            .anchorString(anchor)
-            .anchorUnits(UNITS)
-            .anchorXOffset("0")
-            .anchorYOffset("0");
+                .documentId(DOCUMENT_ID)
+                .tabLabel(tabLabel)
+                .anchorString(anchor)
+                .anchorUnits(UNITS)
+                .anchorXOffset("0")
+                .anchorYOffset("0");
     }
 }
